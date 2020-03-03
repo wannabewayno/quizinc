2d88f2091f256be5f9ba164930aa01e166e22f99
Wayne <wayne.c.griffiths@gmail.com>
2020-03-04 00:11:50 +0800

    renamed the validateQuestion functon to the checkAndUpdate function, pushed code into a seperate function called validate for readability

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,102 +8,130 @@ var answerBox = document.getElementById("answer-box");
 var validationBox = document.getElementById("validation-box");
 var validationContent =document.getElementById("validation-content");
 var startQuiz = document.getElementById("start-quiz");
+var results = document.getElementById("results");
+var finalScore = document.getElementById("score");
+var scoreButton = document.getElementById("score-button");
+var scoreName = document.getElementById("score-name");
 
-//Quiz Source Object
+//Quiz Source Object, contains all quiz questiosn and allowed responses.
 quizSource = {
     question1:{
         question:"how do you define a function argument?",
         correctAnswer:["With rounded brackets ()"],
-        incorrectAnswers:["With curly Brackets {}","With square brackets []","With Double quotes"]
+        incorrectOptions:["With curly Brackets {}","With square brackets []","With Double quotes"]
     },
     question2:{
         question:"what operator do you use to indicate the end of a line",
         correctAnswer:["Semicolon ; "],
-        incorrectAnswers:["Colon : ","Asterix * ","dollar sign $ "]
+        incorrectOptions:["Colon : ","Asterix * ","dollar sign $ "]
     },
     question3:{
         question:"who is who in the zoo?",
         correctAnswer:["Denise"],
-        incorrectAnswers:["Karen","Fran","quasimoto"]
+        incorrectOptions:["Karen","Fran","quasimoto"]
     }
 }   
 
 
 //Variables
-var time = 1;
-var questionNumber = 1;
+var time = 75;
+var questionNumber;
+var popup;
+var score;
 
 //functions
 function startTimer(){
     interval = setInterval(function(){
         time--;
-        console.log(time);
-        if (time === 0){
+        if (time <= 0){
             clearInterval(interval);
+            switchToResults();
+            timeLeft.textContent = ""
+            return;
         }
         timeLeft.textContent = time;
     },1000);
 }
 
 
-function collapsePreface(){
+function switchToQuiz(){
     preface.setAttribute("class", "hide");
-    quizContent.setAttribute("class", "show")
+    quizContent.setAttribute("class", "show");
+}
+
+
+function switchToResults(){
+    quizContent.setAttribute("class", "hide");
+    results.setAttribute("class", "show");
+    finalScore.textContent = score;
 }
 
 
 function startQuizProcess(){
+    score = 0;
+    questionNumber = 1
     startTimer();
-    collapsePreface();
+    switchToQuiz();
     createQuestion(questionNumber);
 }
 
 
-
 function createQuestion(questionNumber) {
-    var selector ="question"+questionNumber;
-    var thisQuestion = quizSource[selector];
-    answers = [];
-    answers = answers.concat(thisQuestion.incorrectAnswers, thisQuestion.correctAnswer)
-    var iterationCount = answers.length
+    var selectQuestion ="question"+questionNumber;
+    var thisQuestion = quizSource[selectQuestion];
 
+    options = [];
+    options = options.concat(thisQuestion.incorrectOptions, thisQuestion.correctAnswer)
+
+    var iterationCount = options.length
     for (let i = 0; i < iterationCount; i++) {
-        var randomNumber = Math.floor(Math.random()*answers.length)
+        var randomNumber = Math.floor(Math.random()*options.length)
         dlEl = document.createElement("button");
-        dlEl.textContent = answers[randomNumber];
-        answers.splice(randomNumber,1);
+        dlEl.textContent = options[randomNumber];
+        options.splice(randomNumber,1);
         answerBox.appendChild(dlEl);
     }
     question.textContent = thisQuestion.question;
 }
 
-function validateQuestion(){
-    var element = event.target;
-    var parent = element.parentElement;
-    selector = "question"+questionNumber
 
-    if (element.matches("button")){
+function validate(answer,whichQuestion) {
+    validationBox.setAttribute("class","show")
+    if(answer.textContent === quizSource[whichQuestion].correctAnswer[0]){
+        validationContent.textContent = "Correct!"
+        score++;
+    } else {
+        validationContent.textContent = "no no no, you naughty goose"
+        time -= 10;
+    }
+
+    clearInterval(popup);
+    popup = setInterval(function(){
+        validationBox.setAttribute("class","hide")
+        clearInterval(popup);
+    },2000);
+}
+
+
+function checkAndUpdate(event){
+    var answer = event.target;
+    var parent = answer.parentElement;
+    whichQuestion = "question"+questionNumber
+
+    if (answer.matches("button")){
         removeChildren(parent);
-        validationBox.setAttribute("class","show")
+        validate(answer,whichQuestion);
 
-        if(element.textContent === quizSource[selector].correctAnswer[0]){
-            validationContent.textContent = "Correct!"
-        } else {
-            validationContent.textContent = "Wrong bitch!"
-        }
-
-        if(questionNumber === Object.keys(quizSource).length){
-            return
+        numberOfQuestions = Object.keys(quizSource).length
+        if(questionNumber === numberOfQuestions) {
+            switchToResults();
+            return;
         }
         createQuestion(questionNumber+1);
         questionNumber++;
-
-        var popup = setInterval(function(){
-            validationBox.setAttribute("class","hide")
-            clearInterval(popup);
-        },2000);
     }
 }
+
 
 function removeChildren(parent){
     iterationCount = parent.childElementCount
@@ -115,4 +143,4 @@ function removeChildren(parent){
 
 //event listeners
 startQuiz.addEventListener("click", startQuizProcess);
-answerBox.addEventListener("click", validateQuestion);
+answerBox.addEventListener("click", checkAndUpdate);
