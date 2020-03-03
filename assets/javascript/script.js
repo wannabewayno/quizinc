6a30798c53a2e82d8bb3e9f8de1e6216a8440d2c
Wayne <wayne.c.griffiths@gmail.com>
2020-03-03 14:18:00 +0800

    created html handles in javascript for all elements needing to be modified

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("time");
+var preface = document.getElementById("preface");
+var quizContent = document.getElementById("quiz-content");
+var question = document.getElementById("question");
+var answerBox = document.getElementById("answer-box");
+var validationBox = document.getElementById("validation-box");
+var validationContent =document.getElementById("validation-content");
