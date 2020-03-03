a43b175325482568f3e1ad874ef210598ef148ab
Wayne <wayne.c.griffiths@gmail.com>
2020-03-03 14:30:44 +0800

    ADDED: a timer to the script.js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+// Document Handles
 var timer = document.getElementById("timer");
 var timeLeft = document.getElementById("time");
 var preface = document.getElementById("preface");
@@ -6,3 +7,20 @@ var question = document.getElementById("question");
 var answerBox = document.getElementById("answer-box");
 var validationBox = document.getElementById("validation-box");
 var validationContent =document.getElementById("validation-content");
+
+//Variables
+var time = 10;
+
+//functions
+
+function startTimer(){
+    interval = setInterval(function(){
+        time--;
+        console.log(time);
+        if (time === 0){
+            clearInterval(interval);
+        }
+    },1000);
+}
+
+//event listeners
