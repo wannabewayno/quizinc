f6ac35a9682529b12cd8e3fe37fa7b49f9a570cf
Wayne <wayne.c.griffiths@gmail.com>
2020-03-03 22:42:05 +0800

    added an object that holds all quiz information with semantic fields, updated html with a css class to display variables when called for via JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,11 +9,31 @@ var validationBox = document.getElementById("validation-box");
 var validationContent =document.getElementById("validation-content");
 var startQuiz = document.getElementById("start-quiz");
 
+//Quiz Source Object
+quizSource = {
+    question1:{
+        question:"how do you define a function argument?",
+        correctAnswer:["With rounded brackets ()"],
+        incorrectAnswers:["With curly Brackets {}","With square brackets []","With Double quotes"]
+    },
+    question2:{
+        question:"what operator do you use to indicate the end of a line",
+        correctAnswer:["Semicolon ; "],
+        incorrectAnswers:["Colon : ","Asterix * ","dollar sign $ "]
+    },
+    question3:{
+        question:"who is who in the zoo?",
+        correctAnswer:["Denise"],
+        incorrectAnswers:["Karen","Fran","quasimoto"]
+    }
+}   
+
+
 //Variables
-var time = 10;
+var time = 1;
+var questionNumber = 1;
 
 //functions
-
 function startTimer(){
     interval = setInterval(function(){
         time--;
